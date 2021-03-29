b44018d9fbfdbc459eea30b93337fe3dc507e4a6
Ivan Vazquez <raul.vaz318@gmail.com>
2021-03-29 16:24:48 -0600

    consulta de reporte y alerta de registrar usuario

--- src/app/shared/servicesshared/consultarreporte.service.ts
+++ src/app/shared/servicesshared/consultarreporte.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { HttpClientService } from 'src/app/implementation/http-client.service';
 import { RespuestaSolicitud } from 'src/app/models/respuestaSolicitud.model';
 import { environment } from 'src/environments/environment';
+import { Reporte } from '../models/respuestaReporte.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultarreporteService {
+  
   Respuesta: RespuestaSolicitud;
+  Report = new BehaviorSubject<Reporte[]>(null);
+  arregloReportes:Reporte[] =[];
   
   constructor(
     private http: HttpClientService
@@ -24,10 +29,15 @@ consultaReporte(){
         this.Respuesta = res;
         if (this.Respuesta.Codigo ==1) {
           console.log(this.Respuesta);
+          let resReporte: any = this.Respuesta.Data;
+
+          resReporte.forEach((reportes) => {
+          this.arregloReportes.push(reportes);            
+          });
         }
       }
     }
-  )
+  );
 
 }
 
